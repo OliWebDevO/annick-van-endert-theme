36d3dbc677504502df7e7fc90ab50136a92628b5
OliWebDevO <oliver-vdb@hotmail.com>
2026-02-21 20:08:30 +0100

    css cache

--- partials/header.php
+++ partials/header.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>body{opacity:0;background:#000}</style>
-    <link rel="stylesheet" href="<?php bloginfo("template_url")?>/styles/styles.css?<?php echo time(); ?>">
+    <style>body:not(.loaded){opacity:0;background:#000}</style>
+    <link rel="stylesheet" href="<?php bloginfo("template_url")?>/styles/styles.css?v=1.0">
       <!-- Google Web Fonts -->
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
